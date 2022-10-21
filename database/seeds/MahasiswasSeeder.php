e73ef8581190803d8c091231a07fc80f85634d8c
AceTaker <62095930+AceTaker@users.noreply.github.com>
2022-10-21 14:48:04 +0800

    Add files via upload
    Commit Pertama

---
+++ database/seeds/MahasiswasSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class MahasiswasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('mahasiswas')->insert([
+            'id' => 1,
+            'nim' => 19110163,
+            'nama' => 'Melky Hermansyah',
+            'tempat_lahir' => 'Barito Kuala',
+            'tanggal_lahir' => date('Y-m-d'),
+            'alamat' => 'TPO',
+            'id_prodi' => 1,
+            'id_kelas' => 1,
+            'agama' => 'Islam',
+            'telp' => '0895701151217',
+            'email' => 'melkyherman29@gmail.com',
+            'jenis_kelamin' => 'L',
+            'photo' => "",
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
